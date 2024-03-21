0bacd3ce58e0d7c2861c0cc81c4f053d0e012c56
Jarraya-Ahmed <ajarraya@strategya2ai.com>
2024-03-21 12:05:12 +0100

    generate  tasks  resolver

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { TasksResolver } from './tasks.resolver';
 
 @Module({
-  providers: [TasksService]
+  providers: [TasksService, TasksResolver]
 })
 export class TasksModule {}
