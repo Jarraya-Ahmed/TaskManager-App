4b0736302fbfea960343cf96ac2a5ba8e00a1a8f
Jarraya-Ahmed <ajarraya@strategya2ai.com>
2024-03-21 12:41:37 +0100

    create task function that retrieve tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    async findAll(): Promise<Task[]> {
+        const task= new Task();
+        task.id=1;
+        task.title="Create Demo Task Management app";
+        return [task]
+    }
+}
